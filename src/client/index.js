2024453ccd55d9f6005d4c428099a0fd43f8ffa6
Patrick Schubert <patrick-schubert@gmx.de>
2020-05-29 02:09:54 +0200

    hopefully fixed pathspec error

--- src/client/index.js
+++ src/client/index.js
@@ -1,9 +1,16 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
 
+// Styles
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
+
 console.log(checkForName);
 
-alert("I EXIST");
-
-alert("I EXIST");
-alert("I EXIST");
+export {
+    checkForName,
+    handleSubmit
+}
--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,15 @@
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+var aylien = require("aylien_textapi");
 
 const app = express()
+
+// set aylien API credentias
+var textapi = new aylien({
+    application_id: process.env.API_ID,
+    application_key: process.env.API_KEY
+});
 
 app.use(express.static('dist'))
 
@@ -14,9 +21,23 @@ app.get('/', function (req, res) {
 
 // designates what port the app will listen to for incoming requests
 app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+    console.log('Example app listening on port 8090!')
 })
 
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
+
+// // POST ROUTE
+
+// app.get('/test', function (req, res) {
+//     console.log(req.body);
+//     res.send(req.body);
+//     res.send(mockAPIResponse)
+//     textapi.sentiment({
+//         'url': req.body.text
+//       }, function(error, response) {
+//           res.send(response)
+//           console.log(response)
+//       });
+// })
